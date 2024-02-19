0af81be2ce4b89d08f6f1220ee39e9e61999dfb5
Tufail Dar <tufaildar2016@gmail.com>
2024-02-19 12:28:06 +0530

    Refactor code to fetch questions from JSON server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Error from "./Error.js";
+import Loader from "./Loader.js";
+import StartScreen from "./StartScreen.js";
+import Question from "./Question.js";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  const initialState = {
+    questions: [],
+    status: "loading",
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataReceived":
+        return {
+          ...state,
+          questions: action.payload,
+          status: "ready",
+        };
+      case "dataFailed":
+        return {
+          ...state,
+          status: "error",
+        };
+
+      case "start":
+        return {
+          ...state,
+          status: "active",
+        };
+      default:
+        throw new Error("Unknown Error");
+    }
+  }
+
+  const [{ status, questions }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question ?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
